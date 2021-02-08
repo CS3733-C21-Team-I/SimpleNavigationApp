c5aef3e9f176c233e140138553c5d078e71baf2e
James Doherty <jadoherty@wpi.edu>
2021-02-08 14:09:22 -0500

    Added NavDatabase Singleton skelleton

--- empty-project/src/main/java/edu/wpi/ithorian/database/NavDatabaseManager.java
+++ empty-project/src/main/java/edu/wpi/ithorian/database/NavDatabaseManager.java
@@ -1,2 +1,22 @@
-package edu.wpi.ithorian.database;public class NavDatabaseManager {
+package edu.wpi.ithorian.database;
+
+public class NavDatabaseManager {
+
+    private static final String DB_URL = "jdbc:derby:memory:navDB;create=true";
+
+    private static NavDatabaseManager ourInstance;
+
+    public static NavDatabaseManager getInstance() {
+        return ourInstance;
+    }
+
+    public static void init() {
+        ourInstance = new NavDatabaseManager();
+    }
+
+    private DatabaseRef databaseRef;
+
+    private NavDatabaseManager() {
+        databaseRef = DatabaseRef.getConnection(DB_URL);
+    }
 }
