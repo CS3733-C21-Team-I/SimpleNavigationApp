f6b04ad54d0f7fee73af72f8666697f533903bbd
James Doherty <jadoherty@wpi.edu>
2021-02-08 14:40:11 -0500

    Fixed native databse generation

--- empty-project/src/main/java/edu/wpi/ithorian/database/NavDatabaseManager.java
+++ empty-project/src/main/java/edu/wpi/ithorian/database/NavDatabaseManager.java
@@ -2,21 +2,21 @@ package edu.wpi.ithorian.database;
 
 public class NavDatabaseManager {
 
-    private static final String DB_URL = "jdbc:derby:memory:navDB;create=true";
+  private static final String DB_URL = "jdbc:derby:navDB;create=true";
 
-    private static NavDatabaseManager ourInstance;
+  private static NavDatabaseManager ourInstance;
 
-    public static NavDatabaseManager getInstance() {
-        return ourInstance;
-    }
+  public static NavDatabaseManager getInstance() {
+    return ourInstance;
+  }
 
-    public static void init() {
-        ourInstance = new NavDatabaseManager();
-    }
+  public static void init() {
+    ourInstance = new NavDatabaseManager();
+  }
 
-    private DatabaseRef databaseRef;
+  private DatabaseRef databaseRef;
 
-    private NavDatabaseManager() {
-        databaseRef = DatabaseRef.getConnection(DB_URL);
-    }
+  private NavDatabaseManager() {
+    databaseRef = DatabaseRef.getConnection(DB_URL);
+  }
 }
