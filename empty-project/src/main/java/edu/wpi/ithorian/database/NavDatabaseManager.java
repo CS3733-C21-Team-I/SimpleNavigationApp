f3103f340f093c0a75626579d96c27bbfa850c82
huynh <duybaohuynh1312@gmail.com>
2021-02-09 18:09:37 -0500

    Tweak SQL queries for drop and add tables

--- empty-project/src/main/java/edu/wpi/ithorian/database/NavDatabaseManager.java
+++ empty-project/src/main/java/edu/wpi/ithorian/database/NavDatabaseManager.java
@@ -1,10 +1,15 @@
 package edu.wpi.ithorian.database;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class NavDatabaseManager extends DatabaseManager {
 
   private static final String DB_URL = "jdbc:derby:navDB";
 
   private static NavDatabaseManager ourInstance;
+  private Connection connection;
 
   public static void init(boolean regen) {
     ourInstance = new NavDatabaseManager(regen);
@@ -14,7 +19,67 @@ public class NavDatabaseManager extends DatabaseManager {
     super(DB_URL, regen);
   }
 
-  void dropTables() {}
+    void dropTables() {
+        try{
+            Statement stmt = connection.createStatement();
+            try {
+                // Drop the Edges table.
+                stmt.execute("DROP TABLE navEdges ");
+            } catch (SQLException ex) {
+                // No need to report an error.
+                // The table simply did not exist.
+            }
 
-  void createTables() {}
+            try {
+                // Drop the Nodes table.
+                stmt.execute("DROP TABLE navNodes ");
+            } catch (SQLException ex) {
+                // No need to report an error.
+                // The table simply did not exist.
+            }
+
+            try {
+                // Drop the Maps table.
+                stmt.execute("DROP TABLE navMaps ");
+            } catch (SQLException ex) {
+                // No need to report an error.
+                // The table simply did not exist.
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    void createTables() {
+        try {
+            Statement stmt = connection.createStatement();
+            try {
+                stmt.execute("CREATE TABLE navMaps(map_ID integer NOT NULL," +
+                        " map_Name varchar(45), floor_Number integer, building_Name varchar(45)," +
+                        " teamAssigned varchar(1), PRIMARY KEY (map_ID))");
+            } catch (SQLException e) {
+                System.out.println("Error generating Map table");
+            }
+
+            try{
+                stmt.execute("CREATE TABLE navNodes(node_ID integer NOT NULL," +
+                        " x_Coord integer NOT NULL, y_Coord integer NOT NULL,is_Named boolean, node_Type varchar(4)," +
+                        "long_Name varchar(45), short_Name varchar(45),map_ID integer, " +
+                        "PRIMARY KEY(node_ID), FOREIGN KEY (map_ID) references navMaps(map_ID))");
+            } catch (SQLException e){
+                System.out.println("Error generating Nodes table");
+            }
+
+            try{
+                stmt.execute("CREATE TABLE navEdges(edge_ID integer NOT NULL, " +
+                        "from_node integer, to_node integer, PRIMARY KEY(edge_ID), " +
+                        "FOREIGN KEY (from_node) references navNodes(node_ID)," +
+                        "FOREIGN KEY (to_node) references navNodes(node_ID))");
+            } catch (SQLException e){
+                System.out.println("Error generating Edges table");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
