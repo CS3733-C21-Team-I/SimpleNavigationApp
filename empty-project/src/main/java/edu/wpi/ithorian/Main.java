f55a925f096e99163ab27aade205a0d24756e5ba
James Doherty <jadoherty@wpi.edu>
2021-02-10 16:50:44 -0500

    Implemented runtime argument to regen database

--- empty-project/src/main/java/edu/wpi/ithorian/Main.java
+++ empty-project/src/main/java/edu/wpi/ithorian/Main.java
@@ -1,11 +1,27 @@
 package edu.wpi.ithorian;
 
 import edu.wpi.ithorian.database.NavDatabaseManager;
+import java.util.Arrays;
 
 public class Main {
 
   public static void main(String[] args) {
-    NavDatabaseManager.init(true);
+
+    if ((args.length > 0) && Arrays.asList(args).contains("regenerate")) {
+      NavDatabaseManager.init(true);
+    } else {
+      NavDatabaseManager.init(false);
+    }
+
+    HospitalMap hm = NavDatabaseManager.getInstance().loadMapFromMemory("Map1");
+
+    for (HospitalMap.Node n : hm.nodes) {
+      System.out.println(n.longToString());
+      System.out.println("\tConnections:");
+      for (HospitalMap.Node c : n.connectedNodes) {
+        System.out.println("\t\t" + c);
+      }
+    }
     // App.launch(App.class, args);
   }
 }
