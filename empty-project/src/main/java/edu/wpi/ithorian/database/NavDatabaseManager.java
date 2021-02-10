2dd7eed4733f83b5470e0e50252cad6f0837927b
James Doherty <jadoherty@wpi.edu>
2021-02-09 19:36:28 -0500

    Implemented loading of Nodes from database, yet to add connection loading

--- empty-project/src/main/java/edu/wpi/ithorian/database/NavDatabaseManager.java
+++ empty-project/src/main/java/edu/wpi/ithorian/database/NavDatabaseManager.java
@@ -3,8 +3,11 @@ package edu.wpi.ithorian.database;
 import edu.wpi.ithorian.HospitalMap;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
 
 public class NavDatabaseManager extends DatabaseManager {
 
@@ -21,7 +24,67 @@ public class NavDatabaseManager extends DatabaseManager {
     super(DB_URL, regen);
   }
 
-  public HospitalMap loadMapFromMemory(int id) {
+  public HospitalMap loadMapFromMemory(String mapId) {
+      ResultSet mapResult = null;
+      try {
+          Statement stmt = connection.createStatement();
+          mapResult = stmt.executeQuery("SELECT * FROM navMaps WHERE map_ID='" + mapId + "'");
+          if (!mapResult.next()) {
+              // Log attempting to acess map not in database
+              return null;
+          }
+      } catch (SQLException e) {
+          // Log error queerying map database
+          return null;
+      }
+
+
+      String mapName, buildingName, teamAssigned;
+      int floor;
+
+      try {
+          mapName = mapResult.getString("map_Name");
+          floor = mapResult.getInt("floor_Number");
+          buildingName = mapResult.getString("building_Name");
+          teamAssigned = mapResult.getString("team_Assigned");
+      } catch (SQLException e) {
+          // Log navMap column names not correct
+          return null;
+      }
+
+      ResultSet nodeResults;
+      try {
+          Statement stmt = connection.createStatement();
+          nodeResults = stmt.executeQuery("SELECT * FROM navNodes WHERE map_ID='" + mapId + "'");
+      } catch (SQLException e) {
+          // Log error queerying map database
+          return null;
+      }
+
+      Set<HospitalMap.Node> nodeSet = new HashSet<>();
+
+      try {
+          while (nodeResults.next()) {
+              String nodeId, nodeType, longname, shortname;
+              int xCoord, yCoord;
+              nodeId = nodeResults.getString("node_ID");
+              nodeType = nodeResults.getString("node_Type");
+              xCoord = nodeResults.getInt("x_Coord");
+              yCoord = nodeResults.getInt("y_Coord");
+              longname = nodeResults.getString("long_Name");
+              shortname = nodeResults.getString("short_Name");
+
+              //TODO edges
+
+              nodeSet.add(new HospitalMap.Node(nodeId, buildingName, nodeType, longname, shortname, teamAssigned, xCoord, yCoord, floor, null));
+
+
+          }
+      } catch (SQLException e) {
+          //More error handling
+          return null;
+      }
+
 
       return null;
   }
