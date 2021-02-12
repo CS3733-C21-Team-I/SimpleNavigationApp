8130ef8fd157389ecbdf927e125680ba3e914975
James Doherty <jadoherty@wpi.edu>
2021-02-12 01:57:22 -0500

    Setup MapEditManager as Singleton

---
+++ empty-project/src/main/java/edu/wpi/ithorian/hospitalMap/mapEditing/MapEditManager.java
@@ -0,0 +1,25 @@
+package edu.wpi.ithorian.hospitalMap.mapEditing;
+
+import edu.wpi.ithorian.hospitalMap.HospitalMap;
+
+import java.util.Map;
+import java.util.Queue;
+
+public class MapEditManager {
+
+    private static MapEditManager ourInstance;
+
+    public static void init() {
+        ourInstance = new MapEditManager();
+    }
+
+    public static MapEditManager getInstance() {
+        return ourInstance;
+    }
+
+
+
+    private MapEditManager() {
+
+    }
+}
