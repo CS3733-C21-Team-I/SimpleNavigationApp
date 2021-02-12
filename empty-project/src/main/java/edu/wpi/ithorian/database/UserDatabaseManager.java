43e58d2940dff5762b302122bdf7875d3270dc2e
James Doherty <jadoherty@wpi.edu>
2021-02-12 16:06:41 -0500

    Bugfixing package problems from UML auto generation

--- empty-project/src/main/java/edu/wpi/ithorian/database/UserDatabaseManager.java
+++ empty-project/src/main/java/edu/wpi/ithorian/database/UserDatabaseManager.java
@@ -1,67 +1,51 @@
 package edu.wpi.ithorian.database;
 
-import edu.wpi.ithorian.database.DatabaseManager;
+import edu.wpi.ithorian.user.User;
 import java.util.List;
 
 public class UserDatabaseManager extends DatabaseManager {
 
-	private static final String DB_URL = "";
-	private UserDatabaseManager ourInstance;
+  private static final String DB_URL = "";
+  private UserDatabaseManager ourInstance;
 
-	/**
-	 * 
-	 * @param regen
-	 */
-	private UserDatabaseManager(boolean regen) {
-		super(DB_URL, regen);
-		// TODO - implement UserDatabaseManager.UserDatabaseManager
-		throw new UnsupportedOperationException();
-	}
+  /** @param regen */
+  private UserDatabaseManager(boolean regen) {
+    super(DB_URL, regen);
+    // TODO - implement UserDatabaseManager.UserDatabaseManager
+    throw new UnsupportedOperationException();
+  }
 
-	/**
-	 * 
-	 * @param regen
-	 */
-	public void init(boolean regen) {
-		// TODO - implement UserDatabaseManager.init
-		throw new UnsupportedOperationException();
-	}
+  /** @param regen */
+  public void init(boolean regen) {
+    // TODO - implement UserDatabaseManager.init
+    throw new UnsupportedOperationException();
+  }
 
-	public UserDatabaseManager getInstance() {
-		// TODO - implement UserDatabaseManager.getInstance
-		throw new UnsupportedOperationException();
-	}
+  public UserDatabaseManager getInstance() {
+    // TODO - implement UserDatabaseManager.getInstance
+    throw new UnsupportedOperationException();
+  }
 
-	/**
-	 * 
-	 * @param userId
-	 */
-	public user.User getUserForId(String userId) {
-		// TODO - implement UserDatabaseManager.getUserForId
-		throw new UnsupportedOperationException();
-	}
+  /** @param userId */
+  public User getUserForId(String userId) {
+    // TODO - implement UserDatabaseManager.getUserForId
+    throw new UnsupportedOperationException();
+  }
 
-	public String getDisplayNameForId() {
-		// TODO - implement UserDatabaseManager.getDisplayNameForId
-		throw new UnsupportedOperationException();
-	}
+  public String getDisplayNameForId() {
+    // TODO - implement UserDatabaseManager.getDisplayNameForId
+    throw new UnsupportedOperationException();
+  }
 
-	/**
-	 * 
-	 * @param roleId
-	 */
-	public List<String> getUsersWithRole(int roleId) {
-		// TODO - implement UserDatabaseManager.getUsersWithRole
-		throw new UnsupportedOperationException();
-	}
+  /** @param roleId */
+  public List<String> getUsersWithRole(int roleId) {
+    // TODO - implement UserDatabaseManager.getUsersWithRole
+    throw new UnsupportedOperationException();
+  }
 
-	@Override
-	protected void createTables() {
+  @Override
+  protected void createTables() {}
 
-	}
-
-	@Override
-	protected void dropTables() {
-
-	}
+  @Override
+  protected void dropTables() {}
 }
--- empty-project/src/main/java/edu/wpi/ithorian/hospitalMap/mapEditing/MapEditManager.java
+++ empty-project/src/main/java/edu/wpi/ithorian/hospitalMap/mapEditing/MapEditManager.java
@@ -1,62 +1,52 @@
 package edu.wpi.ithorian.hospitalMap.mapEditing;
 
+import edu.wpi.ithorian.hospitalMap.HospitalMapNode;
 import java.util.Queue;
 
 public class MapEditManager {
 
-	private MapEditManager ourInstance;
-	private Queue<hospitalMap.mapEditing.NavDataOperation> dataOperations;
+  private MapEditManager ourInstance;
+  private Queue<NavDataOperation> dataOperations;
 
-	private MapEditManager() {
-		// TODO - implement MapEditManager.MapEditManager
-		throw new UnsupportedOperationException();
-	}
+  private MapEditManager() {
+    // TODO - implement MapEditManager.MapEditManager
+    throw new UnsupportedOperationException();
+  }
 
-	/**
-	 * 
-	 * @param regen
-	 */
-	public void init(boolean regen) {
-		// TODO - implement MapEditManager.init
-		throw new UnsupportedOperationException();
-	}
+  /** @param regen */
+  public void init(boolean regen) {
+    // TODO - implement MapEditManager.init
+    throw new UnsupportedOperationException();
+  }
 
-	public MapEditManager getInstance() {
-		// TODO - implement MapEditManager.getInstance
-		throw new UnsupportedOperationException();
-	}
+  public MapEditManager getInstance() {
+    // TODO - implement MapEditManager.getInstance
+    throw new UnsupportedOperationException();
+  }
 
-	/**
-	 * 
-	 * @param node
-	 */
-	public void addNode(hospitalMap.HospitalMapNode node) {
-		// TODO - implement MapEditManager.addNode
-		throw new UnsupportedOperationException();
-	}
+  /** @param node */
+  public void addNode(HospitalMapNode node) {
+    // TODO - implement MapEditManager.addNode
+    throw new UnsupportedOperationException();
+  }
 
-	/**
-	 * 
-	 * @param nodeID
-	 * @param node
-	 */
-	public void editNode(String nodeID, hospitalMap.HospitalMapNode node) {
-		// TODO - implement MapEditManager.editNode
-		throw new UnsupportedOperationException();
-	}
+  /**
+   * @param nodeID
+   * @param node
+   */
+  public void editNode(String nodeID, HospitalMapNode node) {
+    // TODO - implement MapEditManager.editNode
+    throw new UnsupportedOperationException();
+  }
 
-	/**
-	 * 
-	 * @param nodeID
-	 */
-	public void deleteNode(String nodeID) {
-		// TODO - implement MapEditManager.deleteNode
-		throw new UnsupportedOperationException();
-	}
+  /** @param nodeID */
+  public void deleteNode(String nodeID) {
+    // TODO - implement MapEditManager.deleteNode
+    throw new UnsupportedOperationException();
+  }
 
-	public void saveChanges() {
-		// TODO - implement MapEditManager.saveChanges
-		throw new UnsupportedOperationException();
-	}
-
+  public void saveChanges() {
+    // TODO - implement MapEditManager.saveChanges
+    throw new UnsupportedOperationException();
+  }
 }
