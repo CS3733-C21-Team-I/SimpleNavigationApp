eb25e09cf95c6cf4ccfddc9b8695b44a672d1e1d
James Doherty <jadoherty@wpi.edu>
2021-02-12 16:07:16 -0500

    Added toString implementation

--- empty-project/src/main/java/edu/wpi/ithorian/hospitalMap/HospitalMapNode.java
+++ empty-project/src/main/java/edu/wpi/ithorian/hospitalMap/HospitalMapNode.java
@@ -1,35 +1,39 @@
 package edu.wpi.ithorian.hospitalMap;
 
 import edu.wpi.ithorian.pathfinding.GraphNode;
-
 import java.util.List;
 import java.util.Set;
 
 public class HospitalMapNode implements GraphNode<HospitalMapNode> {
 
-	private String id;
-	private Set<HospitalMapNode> connections;
-	private int xCoord;
-	private int yCoord;
+  private String id;
+  private Set<HospitalMapNode> connections;
+  private int xCoord;
+  private int yCoord;
 
-	public HospitalMapNode(String id, int xCoord, int yCoord, Set<HospitalMapNode> connections) {
-		this.id = id;
-		this.connections = connections;
-		this.xCoord = xCoord;
-		this.yCoord = yCoord;
-	}
+  public HospitalMapNode(String id, int xCoord, int yCoord, Set<HospitalMapNode> connections) {
+    this.id = id;
+    this.connections = connections;
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+  }
 
-	@Override
-	public String getID() {
-		return null;
-	}
+  @Override
+  public String getID() {
+    return null;
+  }
 
-	@Override
-	public List<HospitalMapNode> getConnections() {
-		return null;
-	}
+  @Override
+  public List<HospitalMapNode> getConnections() {
+    return null;
+  }
 
-	public void setConnections(Set<HospitalMapNode> connections) {
-		this.connections = connections;
-	}
+  public void setConnections(Set<HospitalMapNode> connections) {
+    this.connections = connections;
+  }
+
+  @Override
+  public String toString() {
+    return "HospitalMapNode{" + "id='" + id + ", xCoord=" + xCoord + ", yCoord=" + yCoord + '}';
+  }
 }
