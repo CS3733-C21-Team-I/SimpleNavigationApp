0da3beb835d33d4935ff6a1c3ca1bd46f9bdf785
James Doherty <jadoherty@wpi.edu>
2021-02-15 14:59:06 -0500

    Added getUser functionality

--- empty-project/src/main/java/edu/wpi/ithorian/database/UserDatabaseManager.java
+++ empty-project/src/main/java/edu/wpi/ithorian/database/UserDatabaseManager.java
@@ -1,9 +1,16 @@
 package edu.wpi.ithorian.database;
 
 import edu.wpi.ithorian.user.User;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static edu.wpi.ithorian.user.User.Role.*;
+import static edu.wpi.ithorian.user.User.Permission.*;
 
 public class UserDatabaseManager extends DatabaseManager {
 
@@ -40,15 +47,90 @@ public class UserDatabaseManager extends DatabaseManager {
 
   }
 
-  /** @param userId */
-  public User getUserForId(String userId) {
+
+
+  /**
+   * Gets a User for their given screenname
+   *
+   * THIS REALLY IS NOT THE SECURE METHOD OF DOING ANYTHING
+   * We should fix this once we have our next push
+   * @param screenName
+   * */
+  public User getUserForId(String screenName) {
     // TODO - implement UserDatabaseManager.getUserForId
-    throw new UnsupportedOperationException();
+
+    int userId;
+
+    try {
+      Statement statement = databaseRef.getConnection().createStatement();
+      ResultSet rs = statement.executeQuery("SELECT * FROM HOSPITAL_USERS WHERE SCREENNAME='"+screenName+"'");
+
+      if (!rs.next()) {
+        //TODO error handling
+        throw new IllegalArgumentException("Attempted to acess nonexistant user");
+      }
+      userId = rs.getInt("USER_ID");
+    } catch (SQLException e) {
+        //TODO Error logging
+        e.printStackTrace();
+        return null;
+    }
+
+    Set<User.Role> rolesSet = new HashSet<>();
+
+    try {
+        Statement statement = databaseRef.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery("SELECT HR.ROLE_NAME FROM USER_TO_ROLE INNER JOIN HOSPITAL_ROLES HR on HR.ROLE_ID = USER_TO_ROLE.ROLE_ID WHERE USER_ID=" + userId);
+
+        while (rs.next()) {
+            rolesSet.add(getRoleForDatabaseName(rs.getString("ROLE_NAME")));
+        }
+
+        if (!rolesSet.contains(User.Role.BASE)) {
+            //TODO Error Logging
+            throw new IllegalStateException("Requested user: " + screenName + " does not contain role BASE");
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+    Set<User.Permission> permissionSet = new HashSet<>();
+
+    try {
+
+      for (User.Role role : rolesSet) {
+          Statement statement = databaseRef.getConnection().createStatement();
+          ResultSet rs = statement.executeQuery("SELECT RP.RESOURCE_NAME FROM ROLE_TO_PERMISSION INNER JOIN RESOURCE_PERMISSIONS RP on ROLE_TO_PERMISSION.RESOURCE_ID = RP.RESOURCE_ID WHERE ROLE_ID=(SELECT ROLE_ID FROM HOSPITAL_ROLES WHERE ROLE_NAME='" + getDatabaseNameForRole(role) + "')");
+
+          permissionSet.add(getPermissionForDatabaseName(rs.getString("RESOURCE_NAME")));
+      }
+    } catch (SQLException e) {
+        //TODO ERROR Logging
+        e.printStackTrace();
+    }
+
+    return new User(userId, screenName, rolesSet, permissionSet);
   }
 
-  public String getDisplayNameForId() {
+    /**
+     * Gets a screename for a user id
+     * @param id
+     * @return
+     */
+  public String getDisplayNameForId(int id) {
     // TODO - implement UserDatabaseManager.getDisplayNameForId
-    throw new UnsupportedOperationException();
+    try {
+        Statement statement = databaseRef.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery("SELECT SCREENNAME FROM HOSPITAL_USERS WHERE USER_ID=" + id);
+        if (!rs.next()) {
+            throw new IllegalArgumentException("Attempted to acess records of nonexistant user");
+        }
+        return rs.getString("SCREENNAME");
+    } catch (SQLException e) {
+        //TODO error handling
+        e.printStackTrace();
+        return "ERROR";
+    }
   }
 
   /** @param roleId */
@@ -182,4 +264,90 @@ public class UserDatabaseManager extends DatabaseManager {
       // The table simply did not exist.
     }
   }
+
+  /**
+   * A hack probably want to implement custom typing?
+   * @param role the role to lookup
+   * @return String coresponding to ROLE_NAME column in database
+   */
+  private String getDatabaseNameForRole(User.Role role) {
+    switch (role) {
+      case BASE:
+        return "BASE";
+      case ADMIN:
+        return "ADMIN";
+      case PATIENT:
+        return "PATIENT";
+      case VISITOR:
+        return "VISITOR";
+      case EMPLOYEE:
+        return "EMPLOYEE";
+      default:
+        return "ERROR";
+    }
+  }
+
+  /**
+   * A hack probably want to implement custom typing?
+   * @param name the role to lookup
+   * @return Role coresponding to ROLE_NAME column in database
+   */
+  private User.Role getRoleForDatabaseName(String name) {
+    switch (name) {
+      case "EMPLOYEE":
+        return EMPLOYEE;
+      case "ADMIN":
+        return ADMIN;
+      case "VISITOR":
+        return VISITOR;
+      case "PATIENT":
+        return PATIENT;
+      case "BASE":
+        return BASE;
+      default:
+        new Exception("ERROR value found in HospitalRoles").printStackTrace();
+        return null;
+    }
+  }
+
+    /**
+     * A hack probably want to implement custom typing?
+     * @param permission the role to lookup
+     * @return String coresponding to ROLE_NAME column in database
+     */
+    private String getDatabaseNameForPermission(User.Permission permission) {
+        switch (permission) {
+            case EDIT_MAP:
+                return "EDIT_MAP";
+            case VIEW_TICKET:
+                return "VIEW_TICKET";
+            case REQUEST_TICKET:
+                return "REQUEST_TICKET";
+            case EDIT_ANNOUNCEMENTS:
+                return "EDIT_ANNOUNCEMENTS";
+            default:
+                return "ERROR";
+        }
+    }
+
+    /**
+     * A hack probably want to implement custom typing?
+     * @param name the role to lookup
+     * @return Role coresponding to ROLE_NAME column in database
+     */
+    private User.Permission getPermissionForDatabaseName(String name) {
+        switch (name) {
+            case "EDIT_MAP":
+                return EDIT_MAP;
+            case "VIEW_TICKET":
+                return VIEW_TICKET;
+            case "REQUEST_TICKET":
+                return REQUEST_TICKET;
+            case "EDIT_ANNOUNCEMENTS":
+                return EDIT_ANNOUNCEMENTS;
+            default:
+                new Exception("ERROR value found in ResourcePermissions").printStackTrace();
+                return null;
+        }
+    }
 }
