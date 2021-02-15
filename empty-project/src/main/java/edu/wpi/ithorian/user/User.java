5a4ee8aceb7e19ed9c88be486f605de04a1faca9
James Doherty <jadoherty@wpi.edu>
2021-02-15 15:15:55 -0500

    Implemented user.hasPermission for testing purposes

--- empty-project/src/main/java/edu/wpi/ithorian/user/User.java
+++ empty-project/src/main/java/edu/wpi/ithorian/user/User.java
@@ -4,35 +4,36 @@ import java.util.Set;
 
 public class User {
 
-	public Role userRole;
-	private Set<Permission> userPermissions;
-	private String name;
-	private int userId;
+  public Set<Role> userRoles;
+  private Set<Permission> userPermissions;
+  private String name;
+  private int userId;
 
-	/**
-	 * 
-	 * @param permission
-	 */
-	public boolean hasPermission(User.Permission permission) {
-		// TODO - implement User.hasPermission
-		throw new UnsupportedOperationException();
-	}
+  public User(int userId, String name, Set<Role> userRoles, Set<Permission> userPermissions) {
+    this.userRoles = userRoles;
+    this.userPermissions = userPermissions;
+    this.name = name;
+    this.userId = userId;
+  }
 
+  /** @param permission */
+  public boolean hasPermission(User.Permission permission) {
+    // TODO - implement User.hasPermission
+    return userPermissions.contains(permission);
+  }
 
-	public enum Role {
-		UNIDENTIFIED,
-		ADMIN,
-		VISITOR,
-		EMPLOYEE,
-		PATIENT
-	}
+  public enum Role {
+    BASE,
+    ADMIN,
+    VISITOR,
+    EMPLOYEE,
+    PATIENT;
+  }
 
-
-	public enum Permission {
-		EDIT_MAP,
-		VIEW_TICKET,
-		REQUEST_TICKET,
-		EDIT_ANNOUNCEMENTS
-	}
-
+  public enum Permission {
+    EDIT_MAP,
+    VIEW_TICKET,
+    REQUEST_TICKET,
+    EDIT_ANNOUNCEMENTS
+  }
 }
