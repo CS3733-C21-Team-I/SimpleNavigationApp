fd1effa1e0734685a3ea04119071ca0d29d12ca4
Mingzhang Zhu <mzhu@wpi.edu>
2021-02-16 00:36:59 -0500

    Create an add button at the bottom for adding new nodes to the csv

--- empty-project/src/main/java/edu/wpi/ithorian/projectCTable/TableAppView.java
+++ empty-project/src/main/java/edu/wpi/ithorian/projectCTable/TableAppView.java
@@ -10,26 +10,26 @@ import javafx.stage.Stage;
 
 public class TableAppView extends Application {
 
-    public static void main(String[] args) {
-        Font.loadFont(TableAppView.class.getResourceAsStream("/OpenSans-Regular.ttf"), 16);
-        launch(args);
-    }
+  public static void main(String[] args) {
+    Font.loadFont(TableAppView.class.getResourceAsStream("/OpenSans-Regular.ttf"), 16);
+    launch(args);
+  }
 
-    @Override
-    public void init() {
-        System.out.println("Starting Up");
-    }
+  @Override
+  public void init() {
+    System.out.println("Starting Up");
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/NodeTable.fxml"));
-        primaryStage.setTitle("Table View");
-        primaryStage.setScene(new Scene(root, 973, 800));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/NodeTable.fxml"));
+    primaryStage.setTitle("Table View");
+    primaryStage.setScene(new Scene(root, 973, 800));
+    primaryStage.show();
+  }
 
-    @Override
-    public void stop() {
-        System.out.println("Shutting Down");
-    }
+  @Override
+  public void stop() {
+    System.out.println("Shutting Down");
+  }
 }
--- empty-project/src/main/java/edu/wpi/ithorian/projectCTable/TableController.java
+++ empty-project/src/main/java/edu/wpi/ithorian/projectCTable/TableController.java
@@ -1,8 +1,46 @@
 package edu.wpi.ithorian.projectCTable;
 
+import com.opencsv.CSVWriter;
+import java.io.*;
 import javafx.fxml.FXML;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class TableController {
+  @FXML TextField Node_ID, Xcod, Ycod, Floor, Building, Type, Long, Short, Team;
+  @FXML Button Add;
 
+  @FXML
+  public void onAddAction() {
+    String nID = Node_ID.getText();
+    String lName = Long.getText();
+    String sName = Short.getText();
+    String tName = Team.getText();
+    String xCord = Xcod.getText();
+    String yCord = Ycod.getText();
+    String fName = Floor.getText();
+    String bName = Building.getText();
+    String type = Type.getText();
+    String is = getCurrentDirectory();
+    is =
+        is.substring(0, is.length() - 1)
+            + "empty-project\\src\\main\\java\\edu\\wpi\\ithorian\\hospitalMap\\mapEditing\\MapINodes.csv";
+    try {
+      CSVWriter writer =
+          new CSVWriter(
+              new FileWriter(is, true), CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER);
+      String line[] = {nID, xCord, yCord, fName, bName, type, lName, sName, tName};
+      writer.writeNext(line);
+      writer.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("File not Found");
+    } catch (IOException e) {
+      System.out.println("IO Exception");
+    }
+  }
+
+  private String getCurrentDirectory() {
+    File file = new File(".");
+    return file.getAbsolutePath();
+  }
 }
