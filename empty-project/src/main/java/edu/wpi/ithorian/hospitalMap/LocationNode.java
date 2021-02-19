e15798d926a080aca4995a37c4d7a2a33d02e4ef
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2021-02-18 19:48:22 -0500

    Integrated table_from_entity (lowercase)

--- empty-project/src/main/java/edu/wpi/ithorian/hospitalMap/LocationNode.java
+++ empty-project/src/main/java/edu/wpi/ithorian/hospitalMap/LocationNode.java
@@ -1,12 +1,28 @@
 package edu.wpi.ithorian.hospitalMap;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LocationNode extends HospitalMapNode {
 
   private String shortName;
   private String longName;
   private String teamAssigned;
+
+  public LocationNode(
+      String id,
+      String mapID,
+      int xCoord,
+      int yCoord,
+      String shortName,
+      String longName,
+      String teamAssigned,
+      List<HospitalMapNode> connections) {
+    super(id, mapID, xCoord, yCoord, connections);
+    this.shortName = shortName;
+    this.longName = longName;
+    this.teamAssigned = teamAssigned;
+  }
 
   public String getShortName() {
     return shortName;
@@ -18,20 +34,5 @@ public class LocationNode extends HospitalMapNode {
 
   public String getTeamAssigned() {
     return teamAssigned;
-  }
-
-  public LocationNode(
-      String id,
-      String mapID,
-      int xCoord,
-      int yCoord,
-      String shortName,
-      String longName,
-      String teamAssigned,
-      ArrayList<HospitalMapNode> connections) {
-    super(id, mapID, xCoord, yCoord, connections);
-    this.shortName = shortName;
-    this.longName = longName;
-    this.teamAssigned = teamAssigned;
   }
 }
