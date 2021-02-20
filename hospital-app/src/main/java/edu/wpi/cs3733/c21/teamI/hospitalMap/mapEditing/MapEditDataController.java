470b2a059ab251cc67eccfc3017587d48af0bab4
James Doherty <jadoherty@wpi.edu>
2021-02-20 17:14:24 -0500

    Implemented Undo redo stack and commands

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/hospitalMap/mapEditing/MapEditDataController.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/hospitalMap/mapEditing/MapEditDataController.java
@@ -2,110 +2,134 @@ package edu.wpi.cs3733.c21.teamI.hospitalMap.mapEditing;
 
 import edu.wpi.cs3733.c21.teamI.hospitalMap.HospitalMap;
 import edu.wpi.cs3733.c21.teamI.hospitalMap.HospitalMapNode;
-import edu.wpi.cs3733.c21.teamI.hospitalMap.LocationNode;
-
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 
 public class MapEditDataController {
 
-	private static MapEditDataController ourInstance;
+  /**
+   * FIFO Stack of MapEdit Commands implementing the command pattern with a stored momento Used for
+   * undo redo operations
+   */
+  private Stack<MapEditCommand> dataOperations;
 
-	public static void init() {
-		ourInstance = new MapEditDataController();
-	}
+  private int undoRedoPointer;
 
-	public static MapEditDataController getInstance() {
-		return ourInstance;
-	}
+  /**
+   * Represents the map that is activelyBeing edited should be referenced from this class's getter
+   * for all UI calls
+   */
+  private HospitalMap activeMap;
 
-	/** FIFO Queue of operations for passing to the databaseManager */
-	private Queue<NavEditOperation> dataOperations;
+  MapEditDataController() {
+    activeMap = null;
+    dataOperations = new Stack<>();
+    undoRedoPointer = -1;
+  }
 
-	/**
-	 * Represents the map that is activelyBeing edited should be referenced from this class's getter
-	 * for all UI calls
-	 */
-	private HospitalMap activeMap;
+  /**
+   * Called to add a entirely new node to the HospitalMap
+   *
+   * @param node the node to be added, currently requires assigning a uniqueNode ID
+   */
+  public void addNode(HospitalMapNode node) {
+    deleteElementsAfterPointer(undoRedoPointer);
+    MapEditCommand command = new AddNodeCommand(this, node);
+    command.execute();
+    dataOperations.push(command);
+    undoRedoPointer++;
+  }
 
-	private MapEditDataController() {
-		activeMap = null;
-		dataOperations = new LinkedList<>();
-	}
+  /**
+   * Called when editing a node. Should be passed a new instance of HospitalMapNode representing the
+   * edited node Usage, editNode(nodeId, new HospitalMapNode(newInfo)
+   *
+   * @param nodeId the Id of the node prior to editing
+   */
+  public void editNode(String nodeId, HospitalMapNode newNode) {
+    deleteElementsAfterPointer(undoRedoPointer);
+    MapEditCommand command = new EditNodeCommand(this, nodeId, newNode);
+    command.execute();
+    dataOperations.push(command);
+    undoRedoPointer++;
+  }
 
-	/**
-	 * Called to add a entirely new node to the HospitalMap
-	 *
-	 * @param node the node to be added, currently requires assigning a uniqueNode ID
-	 */
-	public void addNode(HospitalMapNode node) {
-		// TODO set node id using hashing?
-		dataOperations.add(
-				new NavEditOperation(NavEditOperation.OperationType.ADD_NODE, null, node, null));
-		activeMap.getNodes().add(node);
-	}
+  /**
+   * Called when deleting a node
+   *
+   * @param nodeId the id of the node to be removed
+   */
+  public void deleteNode(String nodeId) {
+    deleteElementsAfterPointer(undoRedoPointer);
+    MapEditCommand command = new RemoveNodeCommand(this, nodeId);
+    command.execute();
+    dataOperations.push(command);
+    undoRedoPointer++;
+  }
 
-	/**
-	 * Called when editing a node. Should be passed a new instance of HospitalMapNode representing the
-	 * edited node Usage, editNode(nodeId, new HospitalMapNode(newInfo)
-	 *
-	 * @param nodeId the Id of the node prior to editing
-	 * @param newNode
-	 */
-	public void editNode(String nodeId, LocationNode newNode) {
-		HospitalMapNode node = activeMap.getNode(nodeId);
-		// TODO reset node.id using hashing?
-		dataOperations.add(
-				new NavEditOperation(NavEditOperation.OperationType.EDIT_NODE, nodeId, node, null));
-	}
+  public void addEdge(String fromNode, String toNode) {
+    deleteElementsAfterPointer(undoRedoPointer);
+    MapEditCommand command = new AddEdgeCommand(this, fromNode, toNode);
+    command.execute();
+    dataOperations.push(command);
+    undoRedoPointer++;
+  }
 
-	/**
-	 * Called when deleting a node
-	 *
-	 * @param nodeId the id of the node to be removed
-	 */
-	public void deleteNode(String nodeId) {
-		dataOperations.add(
-				new NavEditOperation(NavEditOperation.OperationType.DELETE_NODE, nodeId, null, null));
-		activeMap.getNodes().remove(activeMap.getNode(nodeId));
-	}
+  public void deleteEdge(String fromNode, String toNode) {
+    deleteElementsAfterPointer(undoRedoPointer);
+    MapEditCommand command = new DeleteEdgeCommand(this, fromNode, toNode);
+    command.execute();
+    dataOperations.push(command);
+    undoRedoPointer++;
+  }
 
-	public void addEdge(String fromNode, String toNode) {
-		dataOperations.add(
-				new NavEditOperation(NavEditOperation.OperationType.ADD_NODE, fromNode, null, toNode));
-		activeMap.getNode(fromNode).getConnections().add(activeMap.getNode(toNode));
-		activeMap.getNode(toNode).getConnections().add(activeMap.getNode(fromNode));
-	}
+  public HospitalMap getActiveMap() {
+    return activeMap;
+  }
 
-	public void deleteEdge(String fromNode, String toNode) {
-		dataOperations.add(
-				new NavEditOperation(NavEditOperation.OperationType.ADD_NODE, fromNode, null, toNode));
-		activeMap.getNode(fromNode).getConnections().add(activeMap.getNode(toNode));
-		activeMap.getNode(toNode).getConnections().add(activeMap.getNode(fromNode));
-	}
+  /**
+   * Sets the map to be edited, should be called upon entering the map edit UI state with a
+   * HospitalMap loaded from NavDatabaseManager
+   *
+   * @param activeMap The instance of map being edited.
+   */
+  public void setActiveMap(HospitalMap activeMap) {
+    this.activeMap = activeMap;
+  }
 
-	public HospitalMap getActiveMap() { return activeMap; }
+  /**
+   * Called when done editing a single map, will commit the map into the database and sets activeMap
+   * to none
+   */
+  public void saveChanges() {
+    // TODO send to Database
+    activeMap = null;
+    dataOperations = new Stack<>();
+    undoRedoPointer = -1;
+  }
 
-	/**
-	 * Sets the map to be edited, should be called upon entering the map edit UI state with a
-	 * HospitalMap loaded from NavDatabaseManager
-	 *
-	 * @param activeMap The instance of map being edited.
-	 */
-	public void setActiveMap(HospitalMap activeMap) {
-		this.activeMap = activeMap;
-	}
+  public void discardChanges() {
+    activeMap = null;
+    dataOperations = new Stack<>();
+    undoRedoPointer = -1;
+  }
 
-//	+ saveChanges()
-	public void saveChanges() {
-		// TODO send to Database
-		activeMap = null;
-	}
+  public void undo() {
+    MapEditCommand command = dataOperations.get(undoRedoPointer);
+    command.unExecute();
+    undoRedoPointer--;
+  }
 
-//	+ discardChanges()
+  public void redo() {
+    if (undoRedoPointer == dataOperations.size() - 1) return;
+    undoRedoPointer++;
+    MapEditCommand command = dataOperations.get(undoRedoPointer);
+    command.execute();
+  }
 
-// 	+ getClosestNodeToPos(int xPos, int yPos): HospitalMapNode
-
-//	+ undo()
-//	+ redo()
+  private void deleteElementsAfterPointer(int undoRedoPointer) {
+    if (dataOperations.size() < 1) return;
+    for (int i = dataOperations.size() - 1; i > undoRedoPointer; i--) {
+      dataOperations.remove(i);
+    }
+  }
 }
