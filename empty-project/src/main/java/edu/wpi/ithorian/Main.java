5eb0e30e5b11158562a77f31c81556aedd92979e
James Doherty <jadoherty@wpi.edu>
2021-02-19 19:52:39 -0500

    Modified loadMapsFromMemory to work with Map<String, HospitalMap>

--- empty-project/src/main/java/edu/wpi/ithorian/Main.java
+++ empty-project/src/main/java/edu/wpi/ithorian/Main.java
@@ -3,6 +3,7 @@ package edu.wpi.ithorian;
 import edu.wpi.ithorian.database.NavDatabaseManager;
 import edu.wpi.ithorian.hospitalMap.HospitalMap;
 import edu.wpi.ithorian.hospitalMap.HospitalMapCSVBuilder;
+import edu.wpi.ithorian.hospitalMap.HospitalMapNode;
 import edu.wpi.ithorian.hospitalMap.mapEditing.ApplicationView;
 import edu.wpi.ithorian.hospitalMap.mapEditing.MapEditManager;
 import java.util.Arrays;
@@ -12,17 +13,22 @@ import javafx.application.Application;
 public class Main {
 
   public static void main(String[] args) {
-    Map<String, HospitalMap> maps = HospitalMapCSVBuilder.loadCSV("MapINodes.csv", "MapIEdges.csv");
 
     if ((args.length > 0) && Arrays.asList(args).contains("regenerate")) {
       NavDatabaseManager.init(true);
 
+      Map<String, HospitalMap> maps =
+          HospitalMapCSVBuilder.loadCSV("MapINodes.csv", "MapIEdges.csv");
       NavDatabaseManager.getInstance().saveMapsIntoMemory(maps.values());
     } else {
       NavDatabaseManager.init(false);
     }
 
-    HospitalMap map = maps.get("Faulkner 0");
+    HospitalMap map = NavDatabaseManager.getInstance().loadMapsFromMemory().get("Faulkner 0");
+
+    for (HospitalMapNode node : map.getNodes()) {
+      System.out.println(node.getID());
+    }
 
     MapEditManager mapManager = new MapEditManager();
     mapManager.init();
