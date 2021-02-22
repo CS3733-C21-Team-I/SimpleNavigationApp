7b0d1da0f63c90cc24205d1ab52e18fe4d30d2d2
James Doherty <jadoherty@wpi.edu>
2021-02-22 00:43:55 -0500

    Minor formatting fixes

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/ApplicationDataController.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/ApplicationDataController.java
@@ -2,53 +2,55 @@ package edu.wpi.cs3733.c21.teamI;
 
 import edu.wpi.cs3733.c21.teamI.database.UserDatabaseManager;
 import edu.wpi.cs3733.c21.teamI.user.User;
-
 import java.util.HashSet;
 
 public class ApplicationDataController {
 
-    private static ApplicationDataController ourInstance;
+  private static ApplicationDataController ourInstance;
 
-    public static void init() {
-        HashSet<User.Role> baseUserRoles = new HashSet<>();
-        baseUserRoles.add(User.Role.BASE);
-        HashSet<User.Permission> baseUserPermissions = new HashSet<>();
-        User.baseUser = new User(-1, "Unknown User", baseUserRoles, baseUserPermissions);
-        ourInstance = new ApplicationDataController();
-    }
+  public static void init() {
+    HashSet<User.Role> baseUserRoles = new HashSet<>();
+    baseUserRoles.add(User.Role.BASE);
+    HashSet<User.Permission> baseUserPermissions = new HashSet<>();
+    User.baseUser = new User(-1, "Unknown User", baseUserRoles, baseUserPermissions);
+    ourInstance = new ApplicationDataController();
+  }
 
-    public static ApplicationDataController getInstance() {
-        return ourInstance;
-    }
+  public static ApplicationDataController getInstance() {
+    return ourInstance;
+  }
 
-    private User loggedInUser;
+  private User loggedInUser;
 
-    private ApplicationDataController() {
-        loggedInUser = User.baseUser;
-    }
+  private ApplicationDataController() {
+    loggedInUser = User.baseUser;
+  }
 
-    public User getLoggedInUser() {
-        return loggedInUser;
-    }
+  public User getLoggedInUser() {
+    return loggedInUser;
+  }
 
+  /**
+   * Called when logging in a new User
+   *
+   * @param username Username
+   * @param password Password
+   * @return True if valid log in false if not
+   */
+  public boolean logInUser(String username, String password) {
+    User user = UserDatabaseManager.getInstance().getUserForScreenname(username);
 
-    /**
-     * Called when logging in a new User
-     * @param username Username
-     * @param password Password
-     * @return True if valid log in false if not
-     */
-    public boolean logInUser(String username, String password) {
-        User user = UserDatabaseManager.getInstance().getUserForScreenname(username);
+    if (user == null) return false;
 
-        if (user == null)
-            return false;
+    loggedInUser = user;
+    return true;
+  }
 
-        loggedInUser = user;
-        return true;
-    }
+  public boolean isLoggedIn() {
+    return loggedInUser != User.baseUser;
+  }
 
-    public void logOutUser() {
-        loggedInUser = User.baseUser;
-    }
+  public void logOutUser() {
+    loggedInUser = User.baseUser;
+  }
 }
