a032adabac18d2c276b5b8c64f742a53c6fa8ddd
James Doherty <jadoherty@wpi.edu>
2021-02-21 23:38:07 -0500

    Cleaning up cruft in LocationNode

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/hospitalMap/LocationNode.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/hospitalMap/LocationNode.java
@@ -1,31 +1,14 @@
 package edu.wpi.cs3733.c21.teamI.hospitalMap;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class LocationNode extends HospitalMapNode {
 
   private String shortName;
   private String longName;
   private String teamAssigned;
-  private String building;
-  private String nodeType;
-  private int floor;
-  public static List<LocationNode> connectedNodes = new ArrayList<>();
 
   public LocationNode() {};
-
-  public LocationNode(List<String> nodeInit) {
-    this.setId((String) nodeInit.get(0));
-    this.setxCoord(Integer.parseInt((String) nodeInit.get(1)));
-    this.setyCoord(Integer.parseInt((String) nodeInit.get(2)));
-    this.floor = Integer.parseInt((String) nodeInit.get(3));
-    this.building = (String) nodeInit.get(4);
-    this.nodeType = (String) nodeInit.get(5);
-    this.longName = (String) nodeInit.get(6);
-    this.shortName = (String) nodeInit.get(7);
-    this.teamAssigned = (String) nodeInit.get(8);
-  }
 
   public LocationNode(
       String id,
