7d6104de84193743f35435b8e81cf2af22b08ca7
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2021-02-27 23:20:55 -0500

    Changed label to whire and gold, added clock mockup

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/menu/Main.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/menu/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("/fxml/menuFiles/Menu.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("/fxml/ScrollablePicture.fxml"));
     primaryStage.setTitle("Hospital App");
     primaryStage.setScene(new Scene(root));
     primaryStage.show();
---
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/menu/ScrollController.java
@@ -0,0 +1,10 @@
+package edu.wpi.cs3733.c21.teamI.view.menu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ScrollController extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {}
+}
