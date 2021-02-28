77d7ee5bfbde1a458943dcdbbd9bdb2f9530218e
oblaufuss <ocblaufuss@wpi.edu>
2021-02-28 12:36:59 -0500

    Write and test describeDirections

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/pathfinding/TextDirections.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/pathfinding/TextDirections.java
@@ -2,29 +2,74 @@ package edu.wpi.cs3733.c21.teamI.pathfinding;
 
 import edu.wpi.cs3733.c21.teamI.hospitalMap.EuclidianDistCalc;
 import edu.wpi.cs3733.c21.teamI.hospitalMap.HospitalMapNode;
+import edu.wpi.cs3733.c21.teamI.hospitalMap.LocationNode;
 import java.util.ArrayList;
 
 public class TextDirections {
 
-  public static String[] getDirections(EuclidianDistCalc calc, ArrayList<HospitalMapNode> path) {
-    String[] directions = new String[0];
-    // TODO
+  public static ArrayList<String> getDirections(
+      EuclidianDistCalc calc, ArrayList<HospitalMapNode> path) {
+    ArrayList<String> directions = new ArrayList<String>();
+
+    // describe start location
+    HospitalMapNode first = path.get(path.size() - 1);
+    if (first instanceof LocationNode) {
+      directions.add("Begin at " + ((LocationNode) first).getLongName() + ".");
+    }
+
+    for (int i = 1; i < (path.size() - 1); i++) {
+      if (worthDescription(path.get(i))) {
+        directions.add(describeStep(calc, path.get(i - 1), path.get(i), path.get(i + 1)));
+      }
+    }
+    // describe end location
+    HospitalMapNode last = path.get(path.size() - 1);
+    if (last instanceof LocationNode) {
+      directions.add("Continue until you reach " + ((LocationNode) last).getLongName() + ".");
+    }
+
     return directions;
   }
 
-  public String describeStep(
+  public static String describeStep(
       EuclidianDistCalc calc, HospitalMapNode last, HospitalMapNode curr, HospitalMapNode next) {
     String step = "";
+
+    double turn = angleDegrees(calc, last, curr, next);
+    if (turn > 165 || turn < -165) {
+      step += "Continue straight";
+    } else {
+      step += "Take a ";
+      if (!(Math.abs(turn) < 120)) {
+        step += "slight ";
+      }
+      if ((Math.abs(turn) < 60)) {
+        step += "sharp ";
+      }
+      if (turn < 0) {
+        step += "right";
+      } else {
+        step += "left";
+      }
+    }
+
+    if (curr instanceof LocationNode) {
+      step += " at " + ((LocationNode) curr).getLongName();
+    }
+
+    step += ".";
+
     return step;
   }
 
-  // only worth describing if there's multiple possible decisions
-  public boolean isJunction(HospitalMapNode node) {
+  // only worth describing if there's multiple possible decisions at that point OR it's a named
+  // location
+  public static boolean worthDescription(HospitalMapNode node) {
     return node.getConnections().size() > 2;
   }
 
   // returns angle in degrees; negative if on the right hand side, positive if on the left
-  public double angleDegrees(
+  public static double angleDegrees(
       EuclidianDistCalc calc, HospitalMapNode a, HospitalMapNode vertex, HospitalMapNode b) {
     // law of cosines
     double aLen = calc.calculateDistance(vertex, b);
@@ -35,12 +80,12 @@ public class TextDirections {
         (Math.pow(aLen, 2) + Math.pow(bLen, 2) - Math.pow(vertLen, 2)) / (2 * aLen * bLen);
     lawOfCosines = Math.toDegrees(Math.acos(lawOfCosines));
 
-    // determine if on the left side of the line
+    // determine if on the right side of the line
     double crossProduct =
         (vertex.getxCoord() - a.getxCoord()) * (b.getyCoord() - a.getyCoord())
             - (vertex.getyCoord() - a.getyCoord()) * (b.getxCoord() - a.getxCoord());
-    boolean isLeft = crossProduct > 0;
-    if (isLeft) {
+    boolean isRight = crossProduct > 0;
+    if (isRight) {
       lawOfCosines = lawOfCosines * -1;
     }
 
--- hospital-app/src/test/java/edu/wpi/cs3733/c21/teamI/pathfinding/TextDirectionsTest.java
+++ hospital-app/src/test/java/edu/wpi/cs3733/c21/teamI/pathfinding/TextDirectionsTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import edu.wpi.cs3733.c21.teamI.hospitalMap.EuclidianDistCalc;
 import edu.wpi.cs3733.c21.teamI.hospitalMap.HospitalMapNode;
+import edu.wpi.cs3733.c21.teamI.hospitalMap.LocationNode;
 import org.junit.jupiter.api.Test;
 
 class TextDirectionsTest {
@@ -33,21 +34,57 @@ class TextDirectionsTest {
     HospitalMapNode right = new HospitalMapNode("right", "map1", 10, 10, null);
     HospitalMapNode straight = new HospitalMapNode("straight", "map1", 15, 5, null);
 
-    assertEquals(180, test.angleDegrees(helper, start, junction, straight), 0.01);
-    assertEquals(90, test.angleDegrees(helper, start, junction, left), 0.01);
-    assertEquals(-90, test.angleDegrees(helper, start, junction, right), 0.01);
+    assertEquals(180, TextDirections.angleDegrees(helper, start, junction, straight), 0.01);
+    assertEquals(90, TextDirections.angleDegrees(helper, start, junction, left), 0.01);
+    assertEquals(-90, TextDirections.angleDegrees(helper, start, junction, right), 0.01);
 
     // now going back the other way:
-    assertEquals(180, test.angleDegrees(helper, straight, junction, start), 0.01);
-    assertEquals(180, test.angleDegrees(helper, left, junction, right), 0.01);
-    assertEquals(-90, test.angleDegrees(helper, left, junction, start), 0.01);
-    assertEquals(90, test.angleDegrees(helper, right, junction, start), 0.01);
+    assertEquals(180, TextDirections.angleDegrees(helper, straight, junction, start), 0.01);
+    assertEquals(180, TextDirections.angleDegrees(helper, left, junction, right), 0.01);
+    assertEquals(-90, TextDirections.angleDegrees(helper, left, junction, start), 0.01);
+    assertEquals(90, TextDirections.angleDegrees(helper, right, junction, start), 0.01);
 
     // now some gradual lefts and rights:
     HospitalMapNode slightleft = new HospitalMapNode("slightleft", "map1", 17, 1, null);
     HospitalMapNode slightright = new HospitalMapNode("slightright", "map1", 17, 9, null);
 
-    assertEquals(150, test.angleDegrees(helper, start, junction, slightleft), 1);
-    assertEquals(-150, test.angleDegrees(helper, start, junction, slightright), 1);
+    assertEquals(150, TextDirections.angleDegrees(helper, start, junction, slightleft), 1);
+    assertEquals(-150, TextDirections.angleDegrees(helper, start, junction, slightright), 1);
+  }
+
+  @Test
+  public void testDescribeDirections() {
+    EuclidianDistCalc helper = new EuclidianDistCalc();
+
+    // HospitalMapNode junction = new HospitalMapNode( String id, String mapID, int xCoord, int
+    // yCoord, List<HospitalMapNode> connections);
+    LocationNode start =
+        new LocationNode("junction", "map1", 0, 5, "Start", "Starting location", "", null);
+    HospitalMapNode junction = new HospitalMapNode("junction", "map1", 10, 5, null);
+    LocationNode junction2 =
+        new LocationNode("junction", "map1", 10, 5, "Crossroads Cafe", "Crossroads Cafe", "", null);
+    HospitalMapNode left = new HospitalMapNode("left", "map1", 10, 0, null);
+    HospitalMapNode right = new HospitalMapNode("right", "map1", 10, 10, null);
+    HospitalMapNode slightleft = new HospitalMapNode("slightleft", "map1", 17, 1, null);
+    HospitalMapNode slightright = new HospitalMapNode("slightright", "map1", 17, 9, null);
+    HospitalMapNode straight = new HospitalMapNode("straight", "map1", 15, 5, null);
+    LocationNode destination =
+        new LocationNode("destination", "map1", 30, 5, "End", "Hospital Destination", "", null);
+
+    assertEquals("Take a left.", TextDirections.describeStep(helper, start, junction, left));
+    assertEquals("Take a right.", TextDirections.describeStep(helper, start, junction, right));
+
+    assertEquals(
+        "Take a slight left at Crossroads Cafe.",
+        TextDirections.describeStep(helper, start, junction2, slightleft));
+    assertEquals(
+        "Take a slight right at Crossroads Cafe.",
+        TextDirections.describeStep(helper, start, junction2, slightright));
+
+    assertEquals(
+        "Continue straight.", TextDirections.describeStep(helper, start, junction, straight));
+    assertEquals(
+        "Continue straight at Crossroads Cafe.",
+        TextDirections.describeStep(helper, start, junction2, straight));
   }
 }
