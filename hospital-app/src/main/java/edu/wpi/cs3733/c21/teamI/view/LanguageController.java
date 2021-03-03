4016d66f4e7bbcf564dfaf85e4a4547d99a98919
Othniel9 <othnielbondah@gmail.com>
2021-03-03 01:49:37 -0500

    LanguageController update

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/LanguageController.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/LanguageController.java
@@ -3,13 +3,25 @@ package edu.wpi.cs3733.c21.teamI.view;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXTimePicker;
+import edu.wpi.cs3733.c21.teamI.ApplicationDataController;
+import edu.wpi.cs3733.c21.teamI.database.NavDatabaseManager;
+import edu.wpi.cs3733.c21.teamI.database.ServiceTicketDatabaseManager;
+import edu.wpi.cs3733.c21.teamI.database.UserDatabaseManager;
+import edu.wpi.cs3733.c21.teamI.ticket.ServiceTicket;
+import edu.wpi.cs3733.c21.teamI.ticket.ServiceTicketDataController;
+import edu.wpi.cs3733.c21.teamI.user.User;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class LanguageController extends Application {
 
+  ServiceTicket ticket;
   @FXML private JFXTextField langTextfield;
 
   @FXML private JFXTextField langLocationTextfield;
@@ -22,8 +34,13 @@ public class LanguageController extends Application {
 
   @FXML private JFXCheckBox langCheckbox;
 
+  @FXML ListView serviceLocationList, requestAssignedList;
+
+  @FXML AnchorPane background;
+
   @FXML
   void clear(ActionEvent event) {
+
     langTextfield.clear();
     langLocationTextfield.clear();
     langTime.valueProperty().set(null);
@@ -40,19 +57,65 @@ public class LanguageController extends Application {
     String reqString = langReqID.getText();
     String assignedString = langAssignedEmp.getText();
     Boolean isLegal = langCheckbox.isSelected();
-    System.out.println(
-        "Language: "
-            + langString
-            + ", Location: "
-            + locString
-            + ", Time: "
-            + timeString
-            + ", Requester ID: "
-            + reqString
-            + ", Assigned ID: "
-            + assignedString
-            + ", involves legal documebts: "
-            + isLegal);
+    try {
+      int RequestID = ApplicationDataController.getInstance().getLoggedInUser().getUserId();
+      int AssignedID =
+          UserDatabaseManager.getInstance().getUserForScreenname(assignedString).getUserId();
+      ticket =
+          new ServiceTicket(
+              RequestID,
+              ServiceTicket.TicketType.LANGUAGE,
+              NavDatabaseManager.getInstance()
+                  .getMapIdFromLongName(langLocationTextfield.getText()),
+              langTextfield.getText(),
+              false);
+      ServiceTicketDatabaseManager.getInstance().addTicket(ticket);
+    } catch (Exception o) {
+      System.out.println("Error" + o);
+    }
+  }
+
+  private void setupRequestView() {
+    serviceLocationList
+        .getSelectionModel()
+        .selectedItemProperty()
+        .addListener(
+            (ChangeListener<String>)
+                (ov, oldVal, newVal) -> {
+                  langLocationTextfield.setText(newVal);
+                  serviceLocationList.setVisible(false);
+                });
+
+    requestAssignedList
+        .getSelectionModel()
+        .selectedItemProperty()
+        .addListener(
+            (ChangeListener<String>)
+                (ov, oldVal, newVal) -> {
+                  langAssignedEmp.setText(newVal);
+                  requestAssignedList.setVisible(false);
+                });
+
+    background.setOnMouseClicked(
+        t -> {
+          serviceLocationList.setVisible(false);
+          requestAssignedList.setVisible(false);
+        });
+
+    langReqID.setText(ApplicationDataController.getInstance().getLoggedInUser().getName());
+  }
+
+  public void initialize() {
+    setupRequestView();
+  }
+
+  public void lookup(KeyEvent e) {
+    ServiceTicketDataController.lookupNodes(e, serviceLocationList, langLocationTextfield);
+  }
+
+  public void lookupUser(KeyEvent e) {
+    ServiceTicketDataController.lookupUsernames(
+        e, User.Permission.RESPOND_TO_SECURITY, requestAssignedList, langAssignedEmp);
   }
 
   @Override
