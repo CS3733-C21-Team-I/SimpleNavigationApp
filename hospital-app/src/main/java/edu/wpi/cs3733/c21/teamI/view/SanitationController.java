a795aff8493e7a22726d4c888823407b101e4f3d
carleygilmore3@gmail.com <40573151+cgilmore18@users.noreply.github.com>
2021-03-03 12:09:42 -0500

    sanitation request skeleton

---
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/SanitationController.java
@@ -0,0 +1,106 @@
+package edu.wpi.cs3733.c21.teamI.view;
+
+import com.jfoenix.controls.JFXComboBox;
+import edu.wpi.cs3733.c21.teamI.ApplicationDataController;
+import edu.wpi.cs3733.c21.teamI.database.NavDatabaseManager;
+import edu.wpi.cs3733.c21.teamI.database.ServiceTicketDatabaseManager;
+import edu.wpi.cs3733.c21.teamI.database.UserDatabaseManager;
+import edu.wpi.cs3733.c21.teamI.ticket.ServiceTicket;
+import edu.wpi.cs3733.c21.teamI.ticket.ServiceTicketDataController;
+import edu.wpi.cs3733.c21.teamI.user.User;
+import java.io.IOException;
+import javafx.beans.value.ChangeListener;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class SanitationController {
+  ServiceTicket ticket;
+  @FXML TextField sanitationLocation, requesterID, requestAssigned;
+  @FXML TextArea sanitationDetails;
+  @FXML ListView serviceLocationList, requestAssignedList;
+  @FXML AnchorPane background;
+
+  @FXML JFXComboBox securityType;
+
+  public void submit(ActionEvent e) {
+    try {
+      int RequestID = ApplicationDataController.getInstance().getLoggedInUser().getUserId();
+      int AssignedID =
+          UserDatabaseManager.getInstance()
+              .getUserForScreenname(requestAssigned.getText())
+              .getUserId();
+      ticket =
+          new ServiceTicket(
+              RequestID,
+              ServiceTicket.TicketType.SANITATION,
+              NavDatabaseManager.getInstance().getMapIdFromLongName(sanitationLocation.getText()),
+              sanitationDetails.getText(),
+              false);
+      ServiceTicketDatabaseManager.getInstance().addTicket(ticket);
+    } catch (Exception o) {
+      System.out.println("Error" + o);
+    }
+  }
+
+  public void navigate(ActionEvent e) throws IOException {
+    ViewManager.navigate(e);
+  }
+
+  private void setupRequestView() {
+    serviceLocationList
+        .getSelectionModel()
+        .selectedItemProperty()
+        .addListener(
+            (ChangeListener<String>)
+                (ov, oldVal, newVal) -> {
+                  sanitationLocation.setText(newVal);
+                  serviceLocationList.setVisible(false);
+                });
+
+    requestAssignedList
+        .getSelectionModel()
+        .selectedItemProperty()
+        .addListener(
+            (ChangeListener<String>)
+                (ov, oldVal, newVal) -> {
+                  requestAssigned.setText(newVal);
+                  requestAssignedList.setVisible(false);
+                });
+
+    background.setOnMouseClicked(
+        t -> {
+          serviceLocationList.setVisible(false);
+          requestAssignedList.setVisible(false);
+        });
+
+    requesterID.setText(ApplicationDataController.getInstance().getLoggedInUser().getName());
+  }
+
+  public void clear() {
+    sanitationDetails.clear();
+    sanitationLocation.clear();
+    requestAssigned.clear();
+    requesterID.clear();
+    serviceLocationList.setVisible(false);
+    requestAssignedList.setVisible(false);
+  }
+
+  public void initialize() {
+    setupRequestView();
+  }
+
+  public void lookup(KeyEvent e) {
+    ServiceTicketDataController.lookupNodes(e, serviceLocationList, sanitationLocation);
+  }
+
+  public void lookupUser(KeyEvent e) {
+    ServiceTicketDataController.lookupUsernames(
+        e, User.Permission.RESPOND_TO_SANITATION, requestAssignedList, requestAssigned);
+  }
+}
