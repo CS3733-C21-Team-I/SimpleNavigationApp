d9d7a0e43eba4b972de9ebfb1930617b5fc37b2d
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2021-03-03 23:16:13 -0500

    Finished religious feedback form

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/FeedbackViewController.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/FeedbackViewController.java
@@ -1,27 +1,41 @@
 package edu.wpi.cs3733.c21.teamI.view;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXToggleButton;
+import java.awt.*;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 
-import java.io.IOException;
-
 public class FeedbackViewController {
-	@FXML
-	AnchorPane replaceRequest;
+  @FXML AnchorPane replaceRequest;
+  @FXML JFXTextField time;
+  @FXML JFXToggleButton emergency, satisfied, impact;
+  @FXML JFXTextArea description;
 
-	@FXML
-	public void navigate(ActionEvent e) throws IOException {
-		String id = ((JFXButton) e.getSource()).getId();
-		replaceRequest.getChildren().clear();
-		if (id.equals("religiousButton")) {
-			replaceRequest
-					.getChildren()
-					.add(
-							FXMLLoader.load(
-									getClass().getResource("/fxml/serviceRequests/MedicineDelivery.fxml")));
-		}
-	}
+  @FXML
+  public void navigate(ActionEvent e) throws IOException {
+    String id = ((JFXButton) e.getSource()).getId();
+    replaceRequest.getChildren().clear();
+    if (id.equals("religiousButton")) {
+      replaceRequest
+          .getChildren()
+          .add(
+              FXMLLoader.load(
+                  getClass().getResource("/fxml/serviceRequests/ReligiousFeedback.fxml")));
+    }
+  }
+
+  @FXML
+  public void clear(ActionEvent e) {
+    time.clear();
+    emergency.setSelected(false);
+    satisfied.setSelected(false);
+    impact.setSelected(false);
+    description.clear();
+  }
 }
