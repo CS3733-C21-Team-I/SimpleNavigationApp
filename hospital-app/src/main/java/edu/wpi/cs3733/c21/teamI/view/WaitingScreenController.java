aca047720376b4175661f093dea945508a5dff7b
calobo <calobo@wpi.edu>
2021-03-06 18:39:52 -0500

    mobile waiting screen

---
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/WaitingScreenController.java
@@ -0,0 +1,10 @@
+package edu.wpi.cs3733.c21.teamI.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class WaitingScreenController {
+
+  @FXML
+  public void initialize() {}
+}
