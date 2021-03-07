7ba95784aa62cdda0a340488ef4e8cd752a3d810
Mingzhang Zhu <mzhu@wpi.edu>
2021-03-07 13:10:03 -0500

    add Covidrisk boolean function and able to change text

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/NoticeScreenController.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/NoticeScreenController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 
 public class NoticeScreenController {
@@ -16,5 +17,14 @@ public class NoticeScreenController {
     root.getChildren().clear();
     root.getChildren()
         .add(FXMLLoader.load(getClass().getResource("/fxml/MobilePages/PathfindingMobile.fxml")));
+  }
+
+  public void changeText(boolean covidRisk) {
+    Label label2 = (Label) root.lookup("#label2");
+    Label label3 = (Label) root.lookup("#label3");
+    if (!covidRisk) {
+      label2.setText("");
+      label3.setText("You can enter the hospital now and navigate to your destination through the pathfinding function");
+    }
   }
 }
