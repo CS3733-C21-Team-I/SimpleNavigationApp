285d63d7ab95bf6f3af80ffececa2d533e8d4435
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2021-03-06 20:28:03 -0500

    Connected pages

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/NoticeScreenController.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/NoticeScreenController.java
@@ -1,11 +1,20 @@
 package edu.wpi.cs3733.c21.teamI.view;
 
 import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import javafx.fxml.FXML;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.StackPane;
 
 public class NoticeScreenController {
 
-    @FXML
-    JFXButton okBtn;
+  @FXML JFXButton okBtn;
+  @FXML StackPane root;
+
+  @FXML
+  public void goToPathfinding() throws IOException {
+    root.getChildren().clear();
+    root.getChildren()
+        .add(FXMLLoader.load(getClass().getResource("/fxml/MobilePages/PathfindingMobile.fxml")));
+  }
 }
--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/WaitingScreenController.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/WaitingScreenController.java
@@ -1,9 +1,42 @@
 package edu.wpi.cs3733.c21.teamI.view;
 
+import java.io.IOException;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
-public class WaitingScreenController {
+public class WaitingScreenController extends Application {
+
+  @FXML StackPane root;
 
   @FXML
-  public void initialize() {}
+  public void initialize() throws InterruptedException, IOException {
+    Timeline timeline =
+        new Timeline(
+            new KeyFrame(
+                Duration.seconds(5),
+                ev -> {
+                  try {
+                    returnReviewDescision();
+                  } catch (IOException e) {
+                    e.printStackTrace();
+                  }
+                }));
+    timeline.setCycleCount(1);
+    timeline.play();
+  }
+
+  public void returnReviewDescision() throws IOException {
+    root.getChildren().clear();
+    root.getChildren()
+        .add(FXMLLoader.load(getClass().getResource("/fxml/MobilePages/MobileNoticePage.fxml")));
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {}
 }
