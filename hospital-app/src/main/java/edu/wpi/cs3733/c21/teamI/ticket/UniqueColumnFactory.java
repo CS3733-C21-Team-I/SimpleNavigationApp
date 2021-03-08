729ba7a72a4312e9e272a712574e49346ee4bbec
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2021-03-08 13:26:01 -0500

    Everything working except update

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/ticket/UniqueColumnFactory.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/ticket/UniqueColumnFactory.java
@@ -20,14 +20,14 @@ public class UniqueColumnFactory {
       JFXTreeTableColumn<ServiceTicket, String> col1 = makeColumn("Patient Name");
       col1.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(AudioVisualTicket.class))
                   ? new SimpleStringProperty(
                       ((AudioVisualTicket) param.getValue().getValue()).getPatientName())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col2 = makeColumn("Media Type");
       col2.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(AudioVisualTicket.class))
                   ? new SimpleStringProperty(
                       ((AudioVisualTicket) param.getValue().getValue()).getMediaType())
                   : new SimpleStringProperty());
@@ -36,14 +36,14 @@ public class UniqueColumnFactory {
       JFXTreeTableColumn<ServiceTicket, String> col1 = makeColumn("Computer Type");
       col1.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(ComputerTicket.class))
                   ? new SimpleStringProperty(
                       ((ComputerTicket) param.getValue().getValue()).getComputerType())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col2 = makeColumn("Urgency");
       col2.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(ComputerTicket.class))
                   ? new SimpleStringProperty(
                       Boolean.toString(((ComputerTicket) param.getValue().getValue()).isUrgency()))
                   : new SimpleStringProperty());
@@ -52,21 +52,21 @@ public class UniqueColumnFactory {
       JFXTreeTableColumn<ServiceTicket, String> col1 = makeColumn("Language");
       col1.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(LanguageTicket.class))
                   ? new SimpleStringProperty(
                       ((LanguageTicket) param.getValue().getValue()).getLanguage())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col2 = makeColumn("Meeting Time");
       col2.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(LanguageTicket.class))
                   ? new SimpleStringProperty(
                       ((LanguageTicket) param.getValue().getValue()).getMeetingTime())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col3 = makeColumn("Involves Legal");
       col3.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(LanguageTicket.class))
                   ? new SimpleStringProperty(
                       Boolean.toString(
                           ((LanguageTicket) param.getValue().getValue()).isLegalDocs()))
@@ -76,21 +76,21 @@ public class UniqueColumnFactory {
       JFXTreeTableColumn<ServiceTicket, String> col1 = makeColumn("Patient Name");
       col1.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(FloralTicket.class))
                   ? new SimpleStringProperty(
                       ((FloralTicket) param.getValue().getValue()).getPatientName())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col2 = makeColumn("Delivery Date");
       col2.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(FloralTicket.class))
                   ? new SimpleStringProperty(
                       ((FloralTicket) param.getValue().getValue()).getDeliveryDate())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col3 = makeColumn("Delivery Time");
       col3.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(FloralTicket.class))
                   ? new SimpleStringProperty(
                       ((FloralTicket) param.getValue().getValue()).getDeliveryTime())
                   : new SimpleStringProperty());
@@ -99,21 +99,21 @@ public class UniqueColumnFactory {
       JFXTreeTableColumn<ServiceTicket, String> col2 = makeColumn("Pick Up Date");
       col2.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(InternalTransportationTicket.class))
                   ? new SimpleStringProperty(
                       ((InternalTransportationTicket) param.getValue().getValue()).getPickUpDate())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col3 = makeColumn("Pick Up Time");
       col3.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(InternalTransportationTicket.class))
                   ? new SimpleStringProperty(
                       ((InternalTransportationTicket) param.getValue().getValue()).getPickUpTime())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col1 = makeColumn("Emergency");
       col1.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(InternalTransportationTicket.class))
                   ? new SimpleStringProperty(
                       Boolean.toString(
                           ((InternalTransportationTicket) param.getValue().getValue())
@@ -122,7 +122,7 @@ public class UniqueColumnFactory {
       JFXTreeTableColumn<ServiceTicket, String> col4 = makeColumn("Transportation Type");
       col4.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(InternalTransportationTicket.class))
                   ? new SimpleStringProperty(
                       ((InternalTransportationTicket) param.getValue().getValue()).isStretcher()
                           ? "Stretcher"
@@ -133,21 +133,21 @@ public class UniqueColumnFactory {
       JFXTreeTableColumn<ServiceTicket, String> col1 = makeColumn("Pick Up Date");
       col1.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(ExternalTransportationTicket.class))
                   ? new SimpleStringProperty(
                       ((ExternalTransportationTicket) param.getValue().getValue()).getPickUpDate())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col2 = makeColumn("Pick Up Time");
       col2.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(ExternalTransportationTicket.class))
                   ? new SimpleStringProperty(
                       ((ExternalTransportationTicket) param.getValue().getValue()).getPickUpTime())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col3 = makeColumn("Destination");
       col3.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(ExternalTransportationTicket.class))
                   ? new SimpleStringProperty(
                       ((ExternalTransportationTicket) param.getValue().getValue()).getDestination())
                   : new SimpleStringProperty());
@@ -179,35 +179,35 @@ public class UniqueColumnFactory {
       JFXTreeTableColumn<ServiceTicket, String> col1 = makeColumn("Patient Name");
       col1.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(MedicineTicket.class))
                   ? new SimpleStringProperty(
                       ((MedicineTicket) param.getValue().getValue()).getPatientName())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col2 = makeColumn("Date");
       col2.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(MedicineTicket.class))
                   ? new SimpleStringProperty(
                       ((MedicineTicket) param.getValue().getValue()).getDate())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col3 = makeColumn("Time");
       col3.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(MedicineTicket.class))
                   ? new SimpleStringProperty(
                       ((MedicineTicket) param.getValue().getValue()).getTime())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col4 = makeColumn("Drug Name");
       col4.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(MedicineTicket.class))
                   ? new SimpleStringProperty(
                       ((MedicineTicket) param.getValue().getValue()).getDrugName())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col5 = makeColumn("Dose");
       col5.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(MedicineTicket.class))
                   ? new SimpleStringProperty(
                       ((MedicineTicket) param.getValue().getValue()).getDose())
                   : new SimpleStringProperty());
@@ -216,35 +216,35 @@ public class UniqueColumnFactory {
       JFXTreeTableColumn<ServiceTicket, String> col1 = makeColumn("Patient Name");
       col1.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(ReligiousTicket.class))
                   ? new SimpleStringProperty(
                       ((ReligiousTicket) param.getValue().getValue()).getPatientName())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col2 = makeColumn("Religion");
       col2.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(ReligiousTicket.class))
                   ? new SimpleStringProperty(
                       ((ReligiousTicket) param.getValue().getValue()).getReligiousType())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col3 = makeColumn("Religious Denomination");
       col3.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(ReligiousTicket.class))
                   ? new SimpleStringProperty(
                       ((ReligiousTicket) param.getValue().getValue()).getReligiousDenomination())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col4 = makeColumn("Date");
       col4.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(ReligiousTicket.class))
                   ? new SimpleStringProperty(
                       ((ReligiousTicket) param.getValue().getValue()).getDate())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col5 = makeColumn("Time");
       col5.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(ReligiousTicket.class))
                   ? new SimpleStringProperty(
                       ((ReligiousTicket) param.getValue().getValue()).getTime())
                   : new SimpleStringProperty());
@@ -253,14 +253,14 @@ public class UniqueColumnFactory {
       JFXTreeTableColumn<ServiceTicket, String> col1 = makeColumn("Type");
       col1.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(SecurityTicket.class))
                   ? new SimpleStringProperty(
                       ((SecurityTicket) param.getValue().getValue()).getSecurityType())
                   : new SimpleStringProperty());
       JFXTreeTableColumn<ServiceTicket, String> col2 = makeColumn("Emergency");
       col2.setCellValueFactory(
           param ->
-              (param.getValue().getValue().getClass().equals(LaundryTicket.class))
+              (param.getValue().getValue().getClass().equals(SecurityTicket.class))
                   ? new SimpleStringProperty(
                       Boolean.toString(
                           ((SecurityTicket) param.getValue().getValue()).isEmergency()))
