9c4491ced79ea3e04ee0cf43ef97936033fb9e43
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2021-03-11 02:01:21 -0500

    Removed ViewManager getters and setters

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/ViewManager.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/ViewManager.java
@@ -3,21 +3,17 @@ package edu.wpi.cs3733.c21.teamI.view;
 import com.jfoenix.controls.JFXRippler;
 import edu.wpi.cs3733.c21.teamI.ApplicationDataController;
 import edu.wpi.cs3733.c21.teamI.hospitalMap.MapDataEntity;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+
 import java.io.IOException;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
 public class ViewManager {
 
-  private static Group root = null;
-  private static Stage stage = null;
-  private static StackPane replacePane = null;
   static Map<String, String> navigationMap =
       Stream.of(
               new String[][] {
@@ -63,31 +59,5 @@ public class ViewManager {
         MapDataEntity.getNodesSet(true);
       }
     }
-  }
-
-  public ViewManager() {}
-
-  public static StackPane getReplacePane() {
-    return replacePane;
-  }
-
-  public static void setReplacePane(StackPane replacePane) {
-    ViewManager.replacePane = replacePane;
-  }
-
-  public void setRoot(Group root) {
-    ViewManager.root = root;
-  }
-
-  public static void setStage(Stage stage) {
-    ViewManager.stage = stage;
-  }
-
-  public static Stage getStage() {
-    return stage;
-  }
-
-  public static Group getRoot() {
-    return root;
   }
 }
