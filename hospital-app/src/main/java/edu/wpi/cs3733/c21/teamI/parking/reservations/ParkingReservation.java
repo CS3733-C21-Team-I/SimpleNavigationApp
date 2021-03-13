0cb600723def51310c3bc826e89ec87d9d52001b
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2021-03-13 13:31:52 -0500

    Moved reseveration view code

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/parking/reservations/ParkingReservation.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/parking/reservations/ParkingReservation.java
@@ -2,57 +2,49 @@ package edu.wpi.cs3733.c21.teamI.parking.reservations;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class ParkingReservation {
-  private int id;
-  private ParkingCustomer customer;
-  private int slotId;
-  private String slotCode;
-  private Timestamp startTimestamp;
-  private Timestamp endTimestamp;
-  private Date bookingDate;
-  @FXML private Label parkingSlot, start, end, price, barCode, ticketID;
+	private int id;
+	private ParkingCustomer customer;
+	private int slotId;
+	private String slotCode;
+	private Timestamp startTimestamp;
+	private Timestamp endTimestamp;
+	private Date bookingDate;
 
-  public ParkingReservation(
-      int id,
-      int slotId,
-      String slotCode,
-      Timestamp startTimestamp,
-      Timestamp endTimestamp,
-      Date bookingDate) {
-    this.id = id;
-    this.slotId = slotId;
-    this.customer = null;
-    this.slotCode = slotCode;
-    this.startTimestamp = startTimestamp;
-    this.endTimestamp = endTimestamp;
-    this.bookingDate = bookingDate;
-  }
+	public ParkingReservation(
+			int id,
+			int slotId,
+			String slotCode,
+			Timestamp startTimestamp,
+			Timestamp endTimestamp,
+			Date bookingDate) {
+		this.id = id;
+		this.slotId = slotId;
+		this.customer = null;
+		this.slotCode = slotCode;
+		this.startTimestamp = startTimestamp;
+		this.endTimestamp = endTimestamp;
+		this.bookingDate = bookingDate;
+	}
 
-  public ParkingReservation(
-      int id,
-      ParkingCustomer customer,
-      int slotId,
-      String slotCode,
-      Timestamp startTimestamp,
-      Timestamp endTimestamp,
-      Date bookingDate) {
-    this.id = id;
-    this.customer = customer;
-    this.slotId = slotId;
-    this.slotCode = slotCode;
-    this.startTimestamp = startTimestamp;
-    this.endTimestamp = endTimestamp;
-    this.bookingDate = bookingDate;
-  }
-
-  public void redrawTicket(ParkingReservation res) {
-
-  }
-
-  public void submit() {
-    System.out.println("Reservation submitted");
-  }
+	public ParkingReservation(
+			int id,
+			ParkingCustomer customer,
+			int slotId,
+			String slotCode,
+			Timestamp startTimestamp,
+			Timestamp endTimestamp,
+			Date bookingDate) {
+		this.id = id;
+		this.customer = customer;
+		this.slotId = slotId;
+		this.slotCode = slotCode;
+		this.startTimestamp = startTimestamp;
+		this.endTimestamp = endTimestamp;
+		this.bookingDate = bookingDate;
+	}
 }
