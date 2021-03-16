e42d4362c05ec7816c2169fdbdd99fdf0bb6384e
Zacoulou <46580558+Zacoulou@users.noreply.github.com>
2021-03-15 21:56:49 -0400

    FIXED ZOOMING! HALLELUJAAHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/maps/MapPathfindingController.java
+++ hospital-app/src/main/java/edu/wpi/cs3733/c21/teamI/view/maps/MapPathfindingController.java
@@ -119,6 +119,8 @@ public class MapPathfindingController extends MapController {
     if (foundPathExists()) {
       drawCalculatedPath(getFoundPath());
     }
+    // mapPane.getChildren().add(centerPoint);
+    // mapPane.getChildren().add(viewRect);
   }
 
   private void populateDirections(List<DirectionStep> directionSteps) {
@@ -142,7 +144,7 @@ public class MapPathfindingController extends MapController {
       imgView.setFitWidth(20);
       button.setGraphic(imgView);
       button.setPadding(new Insets(10, 10, 10, 10));
-      button.setOnAction((event) -> zoomToStep(step, 50));
+      button.setOnAction((event) -> zoomToStep(step, 0));
       directionsField.getChildren().add(button);
     }
   }
@@ -218,7 +220,8 @@ public class MapPathfindingController extends MapController {
     // Zooms to fit entire path
     if (foundPath.size() >= 2) {
       goToTab(foundPath.get(0).getMapID());
-      zoomToFitNodes(foundPath.get(0), lastNodeOnSameFloor(foundPath), 600);
+      double padding = 100;
+      zoomToFitNodes(foundPath.get(0), lastNodeOnSameFloor(foundPath), padding);
     }
     return foundPath;
   }
@@ -287,7 +290,6 @@ public class MapPathfindingController extends MapController {
 
       showButtonToNextMapOnPath(foundPath);
       populateDirections(TextDirections.getDirectionSteps());
-      //      displayDirections(getFoundPathDescription());
     }
   }
 
